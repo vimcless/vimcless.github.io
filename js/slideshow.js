bb77b21b4863931eb7b8eaf53e235c4ea50553f8
vinicius <vimcless@gmail.com>
2022-09-03 18:03:31 -0300

    Changes

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,11 +6,10 @@ function plusDivs(n) {showDivs(slideIndex += n);}
 function currentDiv(n) {showDivs(slideIndex = n);}
 function showDivs(n) {
   const x = document.getElementsByClassName("mySlides"), dots = document.getElementsByClassName("demo");
-  let i;
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {x[i].style.display = "none";}
-  for (i = 0; i < dots.length; i++) {dots[i].className = dots[i].className.replace(" w3-black", "");}
+  for (let i = 0; i < x.length; i++) {x[i].style.display = "none";}
+  for (let i = 0; i < dots.length; i++) {dots[i].className = dots[i].className.replace(" w3-black", "");}
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-black";
 }
